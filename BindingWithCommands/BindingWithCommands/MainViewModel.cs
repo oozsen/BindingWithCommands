ecfe6e197f2d2c503fcc211e82e2cc1ce40cce7f
Özgür ÖZŞEN <oozsen@anadolu.edu.tr>
2019-11-25 11:41:15 +0300

    Add project files.

---
+++ BindingWithCommands/BindingWithCommands/MainViewModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+
+namespace BindingWithCommands
+{
+    public class MainViewModel : INotifyPropertyChanged
+    {
+        public MainViewModel() { }
+
+        int count = 0;
+        public string DisplayCount => $"You clicked {count} time(s).";
+
+
+        void IncreaseCount()
+        {
+            count++;
+            OnPropertyChanged(nameof(DisplayCount));
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
